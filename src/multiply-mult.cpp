5b5e6cdfcf5546eb9d63cb2a34c23f3f828c4e91
Florian <florian.prive.21@gmail.com>
2018-06-03 13:18:39 +0200

    more macros

--- src/multiply-mult.cpp
+++ src/multiply-mult.cpp
@@ -5,47 +5,51 @@
 /******************************************************************************/
 
 #define ON_VEC(RTYPE, CTYPE) {                                                 \
-  multiply_vec_one< Vector<RTYPE>, CTYPE >(x, as<CTYPE>(val));                 \
+  multiply_vec_mult< Vector<RTYPE>, RTYPE >(x, val);                           \
   break;                                                                       \
 }
 
 #define ON_SUB_VEC(RTYPE, CTYPE) {                                             \
   Vector<RTYPE> x2(x);                                                         \
-  multiply_vec_one(VecAcc<RTYPE, CTYPE>(x2, i), as<CTYPE>(val));               \
+  Vector<RTYPE> val2(val);                                                     \
+  multiply_vec_mult(VecAcc<RTYPE, CTYPE>(x2, i), val2);                        \
   break;                                                                       \
 }
 
 #define ON_SUB_MAT(RTYPE, CTYPE) {                                             \
   Matrix<RTYPE> x2(x);                                                         \
-  multiply_mat_one(MatAcc<RTYPE, CTYPE>(x2, i, j), as<CTYPE>(val));            \
+  Vector<RTYPE> val2(val);                                                     \
+  multiply_mat_mult(MatAcc<RTYPE, CTYPE>(x2, i, j), val2);                     \
   break;                                                                       \
 }
 
 /******************************************************************************/
 
-template <class C_VEC, typename CTYPE>
-void multiply_vec_one(C_VEC x, CTYPE val) {
-  
+template <class C_VEC, int RTYPE>
+void multiply_vec_mult(C_VEC x, const Vector<RTYPE>& val) {
+
+  myassert(x.size() == val.size(), ERROR_DIM);
   int i, n = x.size();
   
   for (i = 0; i < n; i++) 
-    x[i] *= val;
+    x[i] *= val[i];
 }
 
 template <int RTYPE, typename CTYPE>
-void multiply_mat_one(MatAcc<RTYPE, CTYPE> x, CTYPE val) {
+void multiply_mat_mult(MatAcc<RTYPE, CTYPE> x, const Vector<RTYPE>& val) {
   
-  int i, j, n = x.nrow(), m = x.ncol();
+  myassert(x.size() == val.size(), ERROR_DIM);
+  int i, j, k = 0, n = x.nrow(), m = x.ncol();
   
   for (j = 0; j < m; j++)
     for (i = 0; i < n; i++)
-      x(i, j) *= val;
+      x(i, j) *= val[k++];
 }
 
 /******************************************************************************/
 
 // [[Rcpp::export]]
-void multiply_all_one(SEXP x, SEXP i, SEXP j, SEXP val) {
+void multiply_all_mult(SEXP x, SEXP i, SEXP j, SEXP val) {
   
   myassert(i == R_NilValue && j == R_NilValue, ERROR_FATAL);
   
@@ -53,7 +57,7 @@ void multiply_all_one(SEXP x, SEXP i, SEXP j, SEXP val) {
 }
 
 // [[Rcpp::export]]
-void multiply_subvec_one(SEXP x, const NumericVector& i, SEXP j, SEXP val) {
+void multiply_subvec_mult(SEXP x, const NumericVector& i, SEXP j, SEXP val) {
   
   myassert(j == R_NilValue, ERROR_FATAL);
   
@@ -61,10 +65,10 @@ void multiply_subvec_one(SEXP x, const NumericVector& i, SEXP j, SEXP val) {
 }
 
 // [[Rcpp::export]]
-void multiply_submat_one(SEXP x, 
-                         const IntegerVector& i, 
-                         const IntegerVector&  j, 
-                         SEXP val) {
+void multiply_submat_mult(SEXP x,
+                          const IntegerVector& i,
+                          const IntegerVector&  j,
+                          SEXP val) {
   
   DISPATCH(ON_SUB_MAT)
 }
--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/******************************************************************************/
+
 #include <Rcpp.h>
+
+/******************************************************************************/
 
 const char* const ERROR_TYPE =
   "Only 'integer' and 'double' types are supported.";
@@ -15,5 +19,20 @@ const char* const ERROR_FATAL =
 inline void myassert(bool cond, const char *msg) {
   if (!cond) throw Rcpp::exception(msg);
 }
+
+/******************************************************************************/
+
+#define DISPATCH(CALL) {                                                       \
+  switch(TYPEOF(x)) {                                                          \
+  case INTSXP:                                                                 \
+    CALL(INTSXP,  int)                                                         \
+  case REALSXP:                                                                \
+    CALL(REALSXP, double)                                                      \
+  default:                                                                     \
+    throw Rcpp::exception(ERROR_TYPE);                                         \
+  }                                                                            \
+}
+
+/******************************************************************************/
 
 #endif // UTILS_H
